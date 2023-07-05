c89b9a1b005baa4aa16845564f1b9c3edc323548
“tjxt” <tjxt@itcast.cn>
2023-07-05 22:33:07 +0800

    day02我的课表--包括使用mq再提交订单后异步生成我的课表等四个功能

---
+++ tj-learning/src/main/java/com/tianji/learning/mq/LessonChangeListener.java
@@ -0,0 +1,39 @@
+package com.tianji.learning.mq;
+
+import com.tianji.api.dto.trade.OrderBasicDTO;
+import com.tianji.common.constants.MqConstants;
+import com.tianji.common.utils.CollUtils;
+import com.tianji.learning.service.ILearningLessonService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LessonChangeListener {
+    // 构造器的方式注入（构造器通过lombok @RequiredArgsConstructor 生成)
+    private final ILearningLessonService learningLessonService;
+
+    // MQ注解的方式监听交换机
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "learning.lesson.pay.queue", durable = "true"),
+            exchange = @Exchange(name = MqConstants.Exchange.ORDER_EXCHANGE, type = ExchangeTypes.TOPIC),
+            key = MqConstants.Key.ORDER_PAY_KEY
+    ))
+    public void listenLessonPay(OrderBasicDTO order) {
+        // 1.健壮性处理
+        if (order.getOrderId() == null || order.getUserId() == null || CollUtils.isEmpty(order.getCourseIds())){
+            // 数据有误，无需处理
+            log.error("接收到MQ消息有误，订单数据为空");
+            return;
+        }
+        // 2.添加课程
+        log.debug("监听到用户{}的订单{}，需要添加课程{}到课表中", order.getUserId(), order.getOrderId(), order.getCourseIds());
+        learningLessonService.addUserLessons(order.getUserId(), order.getCourseIds());
+    }
+}
