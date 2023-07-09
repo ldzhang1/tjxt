011d49e5bc42b0eab8253db2c475c8d3ad14441d
“tjxt” <tjxt@itcast.cn>
2023-07-09 14:24:44 +0800

    day06-点赞系统：点赞和取消点赞、MQ异步更新点赞数量到其他服务、编写批量根据业务id查询点赞状态的接口给其他服务使用

---
+++ tj-learning/src/main/java/com/tianji/learning/mq/LikedRecordListener.java
@@ -0,0 +1,38 @@
+package com.tianji.learning.mq;
+
+import com.tianji.api.dto.msg.LikedTimesDTO;
+import com.tianji.common.constants.MqConstants;
+import com.tianji.learning.domain.po.InteractionReply;
+import com.tianji.learning.service.IInteractionReplyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LikedRecordListener {
+    private final IInteractionReplyService replyService;
+    /**
+     * QA问答系统  消费者
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "qu.liked.times.queue", durable = "true"),
+            exchange = @Exchange(value = MqConstants.Exchange.LIKE_RECORD_EXCHANGE, type = ExchangeTypes.TOPIC),
+            key =MqConstants.Key.QA_LIKED_TIMES_KEY
+    ))
+    public void onMsg(LikedTimesDTO dto){
+        log.debug("LikeRecordListener 监听消息 {}", dto);
+        InteractionReply reply = replyService.getById(dto.getBizId());
+        if (reply == null){
+            return;
+        }
+        reply.setLikedTimes(dto.getLikedTimes());
+        replyService.updateById(reply);
+    }
+}
